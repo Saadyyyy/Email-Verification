29bcfb9f4dfb5c43678931023965781885710eb4
Laode Saady <laodesaady@Laodes-MacBook-Pro.local>
2024-05-25 01:26:05 +0700

    first commit

---
+++ config/config.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type DatabaseConfig struct {
+	Host     string
+	Port     string
+	Username string
+	Password string
+	DBName   string
+}
+
+func InitializeDatabase() (*sql.DB, error) {
+
+	errE := godotenv.Load()
+	if errE != nil {
+		fmt.Println("Env can connect")
+	}
+
+	dbConfig := DatabaseConfig{}
+	dbConfig.Host = os.Getenv("DBHOST")
+	dbConfig.Port = os.Getenv("DBPORT")
+	dbConfig.Username = os.Getenv("DBUSER")
+	dbConfig.DBName = os.Getenv("DBNAME")
+	dbConfig.Password = os.Getenv("DBPASS")
+	// Konfigurasi koneksi database MySQL dengan GORM
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		dbConfig.Host, dbConfig.Port, dbConfig.Username, dbConfig.Password, dbConfig.DBName)
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Connect To Database")
+	}
+
+	return db, nil
+}
---
+++ models/user.go
@@ -0,0 +1,7 @@
+package models
+
+type Users struct {
+	UserID   int64
+	Username string
+	Password string
+}
